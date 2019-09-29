fb98fcad4171c680d1fb4e4adbb8bfd347a32a0b
GhostDam <49924469+GhostDam@users.noreply.github.com>
2019-09-28 23:01:35 -0500

    created lft rgt buttons

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
-document.addEventListener("DOMContentLoaded", function(){
+$(document).ready(function(){
 var header = `<h1>Selecciona la historia que quieres leer</h1>
-      <ul>
+      <nav>
         <a href="lev.html">Lev's Story</a>
         <a href="blue.html">Blue</a>
         <a href="son.html">Son of the staves of time</a>
-      </ul>`
-
-console.log(header)
-
-function insheader(what) {
-  var where = document.getElementById('head')
-  where.innerHTML = what;
+      </nav>`;
+var bottom = `<button type="button" class="ant">Ant</button>
+              <button type="button" class="sig">Sig</button>`;
+function loadpage(top, bot) {
+  var up = document.getElementById('head')
+  var dn = document.getElementById('botton')
+  up.innerHTML = top;
+  dn.innerHTML = bot;
 }
-insheader(header);
-})
+loadpage(header, bottom);
+
+$(".buttons").hide();
+  $("section").on("click", function(){
+    $(".buttons").toggle();
+  })
+  $(".sig").click(function(){
+    $(".vis").next("section").addClass("vis").prev().removeClass("vis");//se hace visible la tab actual
+  })
+  $(".ant").click(function(){
+    $(".vis").prev("section").addClass("vis").next().removeClass("vis");//se hace visible la tab actual
+  })
+
+
+});//end of the line
