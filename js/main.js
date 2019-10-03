b0d9c6b6b2c9b189aeef4798e7d0d31b92ee40c9
GhostDam <49924469+GhostDam@users.noreply.github.com>
2019-10-03 04:57:10 -0500

    proceture

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
 $(document).ready(function(){
 var header = `<h1>Selecciona la historia que quieres leer</h1>
-      <nav>
-        <a href="lev.html">Lev's Story</a>
-        <a href="blue.html">Blue</a>
-        <a href="son.html">Son of the staves of time</a>
-      </nav>`;
+                  <nav>
+                    <a href="lev.html">Lev's Story</a>
+                    <a href="blue.html">Blue</a>
+                    <a href="son.html">Son of the staves of time</a>
+                  </nav>`;
 var bottom = `<button type="button" class="ant">Ant</button>
               <button type="button" class="sig">Sig</button>`;
-var footer = `<footer>aqui esta el footer ma homs</footer>`;
 function loadpage(top, bot) {
   var up = document.getElementById('head')
   var dn = document.getElementById('botton')
   up.innerHTML = top;
   dn.innerHTML = bot;
 }
+var sct = document.getElementsByTagName('section')
+for (var i =0; i < sct.length; i++) {
+  var atr = sct[i].getAttribute("id");
+  console.log(atr)
+  var li = document.createElement("li")
+  var ref = li.setAttribute("href", `#${atr}`);
+  var fill = document.createTextNode(`${i +1}`)
+  var set = li.appendChild(fill)
+  document.getElementById('set').appendChild(li)
+}
+$("ul li:first-child").addClass("dis")
+$("section:first").addClass("vis")
+
 loadpage(header, bottom);
 
 $(".buttons").hide();
-  $("section").on("click", function(){
+  $("body").on("click", function(){
     $(".buttons").toggle();
   })
-  $(".sig").click(function(){
+  function next(){
     $(".dis").next().addClass("dis").prev().removeClass("dis");
     $(".vis").next("section").addClass("vis").prev().removeClass("vis");//se hace visible la tab actual
-  })
-  $(".ant").click(function(){
+  }
+  function prev(){
     $(".dis").prev().addClass("dis").next().removeClass("dis");
     $(".vis").prev("section").addClass("vis").next().removeClass("vis");//se hace visible la tab actual
-  })
+  }
+  $(".sig").click(next)
+  $(".ant").click(prev)
 $("li").click(function(){
   var ref = $(this).attr("href")
   $("section").removeClass("vis")
@@ -36,5 +50,13 @@ $("li").click(function(){
   $(ref).addClass("vis")
   console.log($(this).attr("href"))
 })
-
+  document.addEventListener("keydown", turnPage)
+  function turnPage(key){
+    if (key.keyCode == 37) {
+      prev()
+      }
+    if (key.keyCode == 39) {
+      next()
+      }
+  }
 });//end of the line
